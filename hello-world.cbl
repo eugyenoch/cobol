50350337eae306b2fe9c35aa275f1793023bf118
David U. Enoch <38564978+eugyenoch@users.noreply.github.com>
2019-06-08 14:04:17 +0100

    Create hello-world.cbl

---
+++ hello-world.cbl
@@ -0,0 +1,15 @@
+      ******************************************************************
+      * Author: Eugy Enoch
+      * Date: 16th May 2019
+      * Purpose: To say Hello world in COBOL
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. GREETING.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM GREETING.
